2d718b3d49e1b28cf1dbf2f33d6d389a3e8a2c8e
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2022-05-12 22:45:25 +0900

    change of launch_evalation.bash

--- envsim/src/visionsim_node.cpp
+++ envsim/src/visionsim_node.cpp
@@ -43,10 +43,13 @@ VisionSim::VisionSim(const ros::NodeHandle &nh, const ros::NodeHandle &pnh)
   simulator_.addModel(ModelThrustTorqueSimple{quad_});
   simulator_.addModel(ModelRigidBody{quad_});
 
+  
   std::string env_cfg_file =
     getenv("FLIGHTMARE_PATH") +
     std::string("/flightpy/configs/vision/config.yaml");
+  ROS_WARN("env cfg file: %s", env_cfg_file.c_str());
 
+  
   vision_env_ptr_ = std::make_unique<flightlib::VisionEnv>(env_cfg_file, 0);
   if (render_) {
     std::string camera_config = ros_param_directory_ + "/camera_config.yaml";
